30f2573290f7de870d5eb11d7ba84eec43f9ff16
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-01-31 11:40:11 -0800

    Added game class.

---
+++ ECommerceSite/Models/Game.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceSite.Models
+{
+    /// <summary>
+    /// Represents a single game available for the purchase
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// Unique identifier for each product
+        /// </summary>
+        [Key]
+        public int GameId { get; set; }
+        /// <summary>
+        /// The title of the games
+        /// </summary>
+        [Required] 
+        public string Title { get; set; }
+        /// <summary>
+        /// The price for the buyer
+        /// </summary>
+        [Range(0, 1000)]     
+        public double Price { get; set; }
+    }
+}
