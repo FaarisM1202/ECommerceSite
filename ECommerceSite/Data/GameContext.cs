c07446157c5290fde41cc6d54c7073fa2ee5087f
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-01-31 12:31:15 -0800

    Added DbContext class.

---
+++ ECommerceSite/Data/GameContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceSite.Data
+{
+    public class GameContext : DbContext
+    {
+        public GameContext(DbContextOptions<GameContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
