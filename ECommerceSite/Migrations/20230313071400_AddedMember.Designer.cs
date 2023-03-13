d135a45d100e613c4db30649a2be9fd3951d26c1
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-03-13 00:27:06 -0700

    Added migration for Member class.

--- ECommerceSite/Migrations/20230313071400_AddedMember.Designer.cs
+++ ECommerceSite/Migrations/20230313071400_AddedMember.Designer.cs
@@ -3,6 +3,7 @@ using ECommerceSite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerceSite.Migrations
 {
     [DbContext(typeof(GameContext))]
-    partial class GameContextModelSnapshot : ModelSnapshot
+    [Migration("20230313071400_AddedMember")]
+    partial class AddedMember
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
