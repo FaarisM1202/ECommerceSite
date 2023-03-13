ad0f7d3173924819a15851bc3e5d2ab832c23872
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-03-13 12:34:40 -0700

    Added  HTTP Post for Register

--- ECommerceSite/Controllers/MembersController.cs
+++ ECommerceSite/Controllers/MembersController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceSite.Data;
+using ECommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace ECommerceSite.Controllers
 {
     public class MembersController : Controller
     {
+        private readonly GameContext _context;
+        public MembersController(GameContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel regModel)
+        {
+            if(ModelState.IsValid)
+            {
+                // Map RegisterViewModel data to Member object
+                Member newMember = new()
+                {
+                    Email = regModel.Email,
+                    Password = regModel.Password
+                };
+
+                _context.Members.Add(newMember);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(regModel);
         }
     }
 }
